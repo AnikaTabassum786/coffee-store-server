e12d30a2e745a1984c70eb8cf876576abc5116ff
AnikaTabassum217 <tabassum.anika579@gmail.com>
2025-05-15 10:04:00 +0600

    Post Done

--- index.js
+++ index.js
@@ -31,6 +31,13 @@ async function run() {
 
   const coffeeCollection = client.db('coffeeDB').collection('coffees')
 
+  app.get('/coffee', async (req,res)=>{
+          const cursor =coffeeCollection.find();
+          const result = await cursor.toArray()
+          res.send(result)
+
+        })
+
     app.post('/coffee', async (req,res)=>{
       const newCoffee = req.body;
       console.log(newCoffee)
