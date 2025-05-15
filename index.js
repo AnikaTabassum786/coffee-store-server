40401eb5cd6dee5bb76aab84680f66a3a78460e4
AnikaTabassum217 <tabassum.anika579@gmail.com>
2025-05-15 13:48:06 +0600

    Get all, get by id, delete ,Post done

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors')
 
 require('dotenv').config()
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -38,11 +38,26 @@ async function run() {
 
         })
 
+    app.get('/coffee/:id', async(req,res)=>{
+      const id =req.params.id;
+      const query={_id: new ObjectId(id)}
+      const result= await coffeeCollection.findOne(query)
+      res.send(result)
+
+    })
+
     app.post('/coffee', async (req,res)=>{
       const newCoffee = req.body;
       console.log(newCoffee)
        const result = await coffeeCollection.insertOne(newCoffee);
        res.send(result)
+    })
+
+    app.delete('/coffee/:id', async (req,res)=>{
+      const id = req.params.id
+      const query={_id: new ObjectId(id)}
+      const result =await coffeeCollection.deleteOne(query)
+      res.send(result)
     })
 
 
