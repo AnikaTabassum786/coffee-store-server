3944173c22c89bbb90db60e17b36e0bd4b095ee6
AnikaTabassum217 <tabassum.anika579@gmail.com>
2025-05-15 19:42:56 +0600

    CRUD operation done

--- index.js
+++ index.js
@@ -53,6 +53,19 @@ async function run() {
        res.send(result)
     })
 
+    app.put('/coffee/:id', async(req,res)=>{
+      const id = req.params.id;
+      const filter={_id: new ObjectId(id)}
+      const options={upsert:true};
+      const updatedCoffee = req.body
+
+      const updateDoc={
+        $set: updatedCoffee
+      }
+      const result = await coffeeCollection.updateOne(filter,updateDoc,options)
+      res.send(result)
+    })
+
     app.delete('/coffee/:id', async (req,res)=>{
       const id = req.params.id
       const query={_id: new ObjectId(id)}
